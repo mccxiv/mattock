72556d73e9049c0f4a887e671b5f2abe73507095
Andrea Stella <adr.stella@gmail.com>
2021-05-22 23:50:30 +0800

    Improve CLI UI

--- packages/mattock/lib/cli.ts
+++ packages/mattock/lib/cli.ts
@@ -1,17 +1,37 @@
 import { getState } from './state'
+import { CONFIG_FILE } from '../constants'
 
 export async function renderCli() {
   const state = await getState()
   const o = console.log
+  const activePlotters = state.plotters.filter(p => p.active)
 
   console.clear()
-  o('___ Plotters: ___________________________________________________________')
-  o('\tpid\tphase\tpct\ttime\tjob')
-  state.plotters.filter(p => p.active).forEach(plotter => {
+  o('------- Plotters: ------------------------------------------------------------')
+  o('   #\tpid\tphase\tpct\ttime\tjob')
+  o('------------------------------------------------------------------------------')
+  activePlotters.forEach((plotter, i) => {
     if (plotter.jobId) {
-      o(`-\t${plotter.pid}\t${plotter.phase}\t${plotter.progress}%\t${plotter.elapsed}\t${plotter.jobName}`)
+      o(`   ${i}\t${plotter.pid}\t${plotter.phase}\t${plotter.progress}%\t${plotter.elapsed}\t${plotter.jobName}`)
     } else {
-      o(`-\t${plotter.pid}\tNo info because it was started externally`)
+      o(`   ${i}\t${plotter.pid}\tNo info because it was started externally`)
     }
   })
+  if (!activePlotters.length) {
+    o('')
+    o('\tNo plotters running.')
+    o('\tIf this is your first time, you need to configure Mattock.')
+    o('')
+    o('\tConfig file:')
+    o('\t' + CONFIG_FILE)
+    o('')
+    o('\tBy default, concurrency on the sample job is set to 0.')
+    o('\tMake sure the temporary and destination directories are ok and')
+    o('\tthen increase the concurrency.')
+    o('\t"concurrent": 0, <- Change this.')
+    o('')
+    o('\tNo need to restart Mattock. It checks the config file every 30 sec.')
+    o('')
+  }
+  o('------------------------------------------------------------------------------')
 }
