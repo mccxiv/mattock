1b8fb55194de885fea0d58e2e54ebf71e7b1a104
Andrea Stella <adr.stella@gmail.com>
2021-05-28 17:55:05 +0800

    Remove stupid dependency

--- packages/mattock/lib/validation.ts
+++ packages/mattock/lib/validation.ts
@@ -1,16 +1,14 @@
 import { MattockConfig } from '../types/types'
+import * as fs from 'fs'
 import * as path from 'path'
 import * as execa from 'execa'
 import * as semver from 'semver'
-import * as drivelist from 'drivelist'
 import { problems } from './state'
 
 export async function isConfigValid(config: MattockConfig): Promise<boolean> {
-  const drives = await drivelist.list()
-
   const results: boolean[] = [
     await validateExecutable(config.chiaExecutable),
-    ...(config.jobs.map(job => validateJob(drives, job)))
+    ...(config.jobs.map(job => validateJob(job)))
   ]
 
   return results.every(res => res)
@@ -30,29 +28,21 @@ export function validateExecutable (executable: string): boolean {
   }
 }
 
-export function validateJob(drives: drivelist.Drive[], job: MattockConfig['jobs'][number]): boolean {
+export function validateJob(job: MattockConfig['jobs'][number]): boolean {
   const nameRegex = /^[-_a-z0-9]{1,10}$/i
   if (!nameRegex.test(job.name)) {
     problems.push('Invalid job name. Rules: 1 to 10 characters, no symbols')
     return false
   }
 
-  const mountPaths = drives.reduce(
-    (acc, current) => {
-      const mountPaths = current.mountpoints.map(mp => path.resolve(mp.path))
-      return [...acc, ...mountPaths]
-    },
-    []
-  )
+  const temp = path.resolve(job.temporaryDirectory)
+  const dest = path.resolve(job.destinationDirectory)
+  const tempValid = createDir(temp)
+  const destValid = createDir(dest)
 
-  const tempResolved = path.resolve(job.temporaryDirectory)
-  const destResolved = path.resolve(job.destinationDirectory)
-  const tempSeemsValid = mountPaths.some(mountPath => tempResolved.startsWith(mountPath))
-  const destSeemsValid = mountPaths.some(mountPath => destResolved.startsWith(mountPath))
-
-  if (!tempSeemsValid || !destSeemsValid) {
-    if (!tempSeemsValid) problems.push(`Temporary directory for ${job.name} seems invalid`)
-    if (!destSeemsValid) problems.push(`Destination directory for ${job.name} seems invalid`)
+  if (!tempValid || !destValid) {
+    if (!tempValid) problems.push(`Unable to access temporary dir for job: ${job.name}.`)
+    if (!destValid) problems.push(`Unable to access destination dir for job: ${job.name}.`)
     return false
   }
 
@@ -60,6 +50,12 @@ export function validateJob(drives: drivelist.Drive[], job: MattockConfig['jobs'
     problems.push(`Job ${job.name} has no concurrency set, it will not start any plotters`)
   }
 
+  return true
+}
+
+function createDir (dirPath: string): boolean {
+  try {fs.mkdirSync(dirPath, {recursive: true})} catch (e) {}
+  try {fs.statSync(dirPath)} catch (e) {return false}
   return true
 }
 
